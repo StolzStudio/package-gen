1a2f2b001028c233a10e4f7b1694ec511f43101e
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-07-12 07:12:16 +1000

    init speaker packet using generator

---
+++ speaker.pm
@@ -0,0 +1,44 @@
+package speaker;
+
+use strict;
+use warnings;
+use speaker;
+
+BEGIN {
+	require Exporter;
+
+	our @ISA = qw(Exporter);
+
+	our @EXPORT_OK = qw(hello_message exit_message);
+}
+
+sub $hello_message {
+	
+}
+
+sub $exit_message {
+	
+}
+
+package speaker;
+
+use strict;
+use warnings;
+use speaker;
+
+BEGIN {
+	require Exporter;
+
+	our @ISA = qw(Exporter);
+
+	our @EXPORT_OK = qw(hello_message exit_message);
+}
+
+sub $hello_message {
+	
+}
+
+sub $exit_message {
+	
+}
+
