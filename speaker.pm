3696e46c85041724fdc8255636fe4d199bcb8332
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-07-17 20:32:17 +1000

    improve speaker

--- speaker.pm
+++ speaker.pm
@@ -8,17 +8,22 @@ BEGIN {
 
 	our @ISA = qw(Exporter);
 
-	our @EXPORT_OK = qw(hello_message exit_message);
+	our @EXPORT_OK = qw(message_hello message_exit message_finish_generate);
 }
 
-sub hello_message {
+sub message_hello {
 	return "glad to meet you in my package module generator\n"
-				."type what you want to generate or read simple tutorial (call -help)\n";
+				. "if you know how work {\n\ttype what you want to generate\n}"
+				. "else {\n\tread readme\n}\n";
 }
 
-sub exit_message {
+sub message_finish_generate {
+	return "package generate ^.^\n";
+}
+
+sub message_exit {
 	return "oh, i hope i help you\n"
-				."see you later ^.^\n";
+				."see you later...\n";
 }
 
 1;
